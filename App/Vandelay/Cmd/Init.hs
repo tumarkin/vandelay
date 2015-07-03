8affb7ca96c4c5def1bdc7a6029fc0537231994c
Robert Tumarkin <r.tumarkin@unsw.edu.au>
2015-07-03 16:13:51 +1000

    init subcommand supports multiple file with cross-referencing

--- App/Vandelay/Cmd/Init.hs
+++ App/Vandelay/Cmd/Init.hs
@@ -1,82 +1,84 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module App.Vandelay.Cmd.Init 
+module App.Vandelay.Cmd.Init
   ( initTemplate
   , SortOptions(..)
+  , SourceFileReferences(..)
   ) where
 
 import App.Vandelay.Core
+import Control.Applicative
+import Control.Arrow
 import Control.Monad
 import Control.Monad.IO.Class
-import Control.Monad.Trans.Either
 import Control.Monad.Trans.Class 
+import Control.Monad.Trans.Either
 import Control.Monad.Trans.RWS
--- import Control.Monad.Trans.Writer
 import Data.List
-import qualified Data.Text as T
--- import Data.Text (Text, pack, unpack)
 import System.FilePath
+import qualified Data.Map as M
+import qualified Data.Text as T
 
+liftEIO :: a -> EIO String a
+liftEIO = return 
+
+liftEIOString :: String -> EIO String String
+liftEIOString = return
 
 
 -- Initialize template
-initTemplate :: String       -- ^ Estimation results filepath
+initTemplate :: [String]       -- ^ Estimation results filepath
              -> Maybe String -- ^ Optional output file (stdout if nothing)
              -> SortOptions 
+             -> SourceFileReferences -- ^ Use abbreviated model names
              -> EIO String () -- ^ Error message or ()
-initTemplate estPath textOutFile sos = do
-  estFile <- safeReadFile estPath
-  (_,_,text) <- runRWST (writeConf >> writeTable >> writeSub ) (estPath, estFile, sos) ()
+initTemplate estPaths textOutFile sos ab = do
+  estFile    <- mapM safeReadFile estPaths
+  (_,_,text) <- runRWST (writeConf >> writeTable >> writeSub ) (InitSetup estPaths estFile sos ab) ()
   
   safeWriteFile textOutFile text 
 
 
-data SortOptions =  
-  SortOptions 
-    { -- | Output models in order of appearance in estimates file if true
-      nosortmodels :: Bool 
-      -- | Output variables in order of appearance in estimates file  if true
-    , nosortvars   :: Bool
+
+
+
+-- -- | Internal data types
+type InitMonad   = RWST InitSetup String () (EIO String)
+data InitSetup   = InitSetup 
+  { dataFilePaths        :: [FilePath]
+  , dataFileContents     :: [FileContent]
+  , sortOptions          :: SortOptions
+  , sourceFileReferences :: SourceFileReferences
+  }
+
+type FileContent = String
+
+data SortOptions = SortOptions 
+    { -- | Output models in order of appearance in estimates file if false
+      sortModels :: Bool 
+      -- | Output variables in order of appearance in estimates file  if false
+    , sortVars   :: Bool
     }
     deriving (Show)
 
-
-
--- | Internal data types
-type InitMonad        = RWST Setup String () (EIO String)
-type Setup            = (DataFilePath, DataFileContents, SortOptions)
-type DataFilePath     = String
-type DataFileContents = String
-
-
-askPath          :: InitMonad DataFilePath
-askContents      :: InitMonad DataFileContents
-askNoSortOptions :: InitMonad SortOptions
-askNoSortModels  :: InitMonad Bool
-askNoSortVars    :: InitMonad Bool
-
-askPath          = asks fst'
-askContents      = asks snd'
-askNoSortOptions = asks trd'
-askNoSortModels  = liftM nosortmodels askNoSortOptions
-askNoSortVars    = liftM nosortvars   askNoSortOptions
-
-fst' (a,_,_) = a
-snd' (_,a,_) = a
-trd' (_,_,a) = a
+data SourceFileReferences = 
+    NoSFR
+  | FullPath
+  | Abbreviation
+  deriving (Show, Read, Eq)
+  
 
 -- | Write the template's configuration section
 writeConf :: InitMonad () 
 writeConf = do
   dfp <- askPath
   let   texFile  = replaceExtension baseFile ".tex"
-        baseFile = takeFileName dfp
+        baseFile = takeFileName . head $ dfp
 
   tellLn   "configuration:"
-  tellLn $ "  data:   " ++ dfp 
+  writeDataFiles
   tellLn   "  models: # List models separated by columns"
   tellLn $ "  tex:    " ++ texFile
-
   tellLn "" 
   tellLn "  # Models are:" 
   writeModels
@@ -90,7 +92,6 @@ writeTable = do
   tellLn "  template: header.tex # Subject to substitutions"
   tellLn "  name: Print Name; code: coded_name; index: 0; surround: (,); format: %03.2f # Variable output. Only code is required"
   tellLn "  latex: \\addlinespace # Source latex code - do not add end lines "
-
   tellLn "" 
   tellLn "  # Variables are:" 
   writeVars
@@ -105,39 +106,157 @@ writeSub = do
 
 
 
--- | Write the variables 
-writeVars :: InitMonad ()
-writeVars = do 
-  contents <- askContents 
-  nosort   <- askNoSortVars
 
-  let sorter = if nosort then sort else id
-      vs     = sorter . stripFilter . map (head . T.splitOn tab) . T.lines . pack $ contents
-  mapM_ (tellLn . indentAndComment) vs
 
--- | Write the models 
+-- | Write the datafiles
+writeDataFiles :: InitMonad ()
+writeDataFiles = do
+  paths <- askPath
+  refs  <- askFileReferences
+  sfr   <- asks sourceFileReferences
+
+  if sfr /= Abbreviation then tellLn . indent . dataStatement . intercalate  ", " =<< askPath 
+                         else let out   = map (indent . dataStatement *** refStatement) $ zip paths refs 
+                              in  mapM_ tellLnWithDataSource =<< lengthenItemRefTuple out
+
+
+
+-- | Write the models and variables 
 writeModels :: InitMonad ()
-writeModels = do
-  contents <- askContents 
-  nosort   <- askNoSortModels
+writeVars   :: InitMonad ()
 
-  let sorter = if nosort then sort else id
-      ms    = sorter . stripFilter . T.splitOn tab . head . T.lines . pack $ contents 
-  mapM_ (tellLn . indentAndComment) ms
+writeModels = writeItem askSortModels modelReferenceTuple
+writeVars   = writeItem askSortVars   varReferenceTuple
+
+
+writeItem :: InitMonad Bool                            -- | Sorting asker
+          -> (String -> String -> [(String, [String])])-- | Item-reference tupling function 
+          -> InitMonad ()
+writeItem askSort tupler = do
+  contents <- askContents 
+  refs     <- askFileReferences
+  sortQ    <- askSort
+
+  let sorter    = if sortQ then id else sort
+      vts       = concat . zipWith tupler
+                  refs $ contents 
+      vs        = sorter . M.toList . M.fromListWith (++) $ reverse vts  --- [(Variable String, [Data Source])]
+      maxVarLen = maximum . map (length . fst) $ vs
+
+      out   =  map ( indentAndComment *** dataSourceStatement ) vs 
+  mapM_ tellLnWithDataSource =<< lengthenItemRefTuple out
+
+
+
+
+
+-- | Model and variable from content
+modelsFromContent :: String   -- | Content
+                  -> [String] -- | Model
+modelsFromContent =  
+  stripFilter . T.splitOn tab . head . T.lines . pack
+
+
+varsFromContent :: String   -- | Content
+                -> [String] -- | [(Variable, Ref)]
+varsFromContent =  
+  stripFilter . map (head . T.splitOn tab) . T.lines . pack
+
+
+itemReferenceTuple :: (String -> [String]) -- | Item Extraction function
+                   -> String -- | Ref
+                   -> String -- | Content
+                   -> [(String, [String])] -- | [(Item, [Ref])]
+itemReferenceTuple f r c = zip (f c) $ repeat [r]
+
+
+varReferenceTuple   = itemReferenceTuple varsFromContent
+modelReferenceTuple = itemReferenceTuple modelsFromContent
+
+-- varReferenceTuple :: String -- | Ref
+--                    -> String -- | Content
+--                    -> [(String, [String])] -- | [(Variable, [Ref])]
+-- varReferenceTuple r c = zip (varsFromContent c) $ repeat [r]
+
+
+-- modelReferenceTuple :: String
+--                     -> String               -- | Content
+--                    -> [(String, [String])] -- | [(Variable, [Ref])]
+-- modelReferenceTuple r c = zip (modelsFromContent c) $ repeat [r]
+
+
+
+
+
+
+
+-- | Reader ask utility functions
+
+askPath           :: InitMonad [FilePath]
+askContents       :: InitMonad [FileContent]
+askContentsRefs   :: InitMonad [(FileContent, String)]
+askSortOptions    :: InitMonad SortOptions
+askSortModels     :: InitMonad Bool
+askSortVars       :: InitMonad Bool
+askFileReferences :: InitMonad [String]
+
+askPath           = asks dataFilePaths 
+askContents       = asks dataFileContents
+askContentsRefs   = zip <$> askContents <*> askFileReferences
+askSortOptions    = asks sortOptions 
+askSortModels     = liftM sortModels askSortOptions
+askSortVars       = liftM sortVars   askSortOptions
+askFileReferences = do
+  a <- asks sourceFileReferences
+  case a of 
+    Abbreviation -> return abbreviations 
+    otherwise    -> asks dataFilePaths
+
+
+
+
 
 
 
 
 
 -- | Text utility functions
+tellLn s = tell $ s ++ "\n"
+tellLnWithDataSource (s,ds) = do
+  b <- asks sourceFileReferences
+  if b /= NoSFR then tellLn (s ++ ds) else tellLn s
+
 stripFilter:: [Text] -> [String] -- Remove spaces, drop blanks
 stripFilter = map unpack . filter (not . T.null) . map T.strip  
 
-tab :: Text
 tab = "\t"
 
+indent:: String -> String
+indent = (++) "  "
+
 indentAndComment :: String -> String
-indentAndComment s = "  # " ++ s
+indentAndComment = indent . (++) "# " 
 
-tellLn s = tell $ s ++ "\n"
+abbreviations = map (\c -> "(" ++ c : ")") ['A'..'Z']
 
+extendString :: Int -> String -> String
+extendString i s | length s > i = s
+                 | otherwise    = s ++ replicate (i - length s) ' ' 
+
+lengthenItemRefTuple :: [(String, String)] 
+                     -> InitMonad [(String,String)]
+lengthenItemRefTuple ts = do
+  sfr <- asks sourceFileReferences
+  if sfr /= NoSFR then let maxlength = maximum . map (length . fst) $ ts
+                       in  return $ map (first (extendString maxlength)) ts
+                       else return ts
+
+-- | Statement creation
+dataStatement :: String -> String
+dataStatement = (++) "data: "
+
+dataSourceStatement :: [String] -> String
+dataSourceStatement rs = unwords [" # In", unwordEnglishList rs]
+
+refStatement :: String -> String
+refStatement s = unwords [" #", s]
