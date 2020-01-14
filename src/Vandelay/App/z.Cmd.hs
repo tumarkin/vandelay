5500391bff524a5db4e0fa28c24926f62038063b
Robert Tumarkin <roberttumarkin@roberts-ipro.ad.unsw.edu.au>
2020-01-14 12:07:07 +1100

    Updating for Stack.

--- src/Vandelay/App/z.Cmd.hs
+++
@@ -1,6 +0,0 @@
-module Vandelay.App.Cmd
-  ( module X
-  ) where
-
-import           Vandelay.App.Cmd.Init as X
-import           Vandelay.App.Cmd.Make as X
--- src/Vandelay/App/z.Template.hs
+++
@@ -1,7 +0,0 @@
-module Vandelay.Template
-  ( module X
-  ) where
-
-import           Vandelay.Template.ParserT as X
--- import           Vandelay.Template.Types   as X
-
